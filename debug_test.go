36a86d4db491a708915e76e261eb63598b199c4a
Matt Wilson <152443343+mwsis@users.noreply.github.com>
2025-08-22 17:31:55 +1000

    Added `File()` to **Debugging** API (#9)
    * added tests for `FileLine()` and `FileLineFunction()`
    
    * * added `File()` to **Debugging** API;
    
    * documentation
    
    * documentation

---
+++ debug_test.go
@@ -0,0 +1,50 @@
+package diagnosticism_test
+
+import (
+	d "github.com/synesissoftware/Diagnosticism.Go"
+
+	"regexp"
+	"testing"
+)
+
+func Test_File(t *testing.T) {
+
+	// NOTE: this regex will likely not work on Windows
+
+	expected := `^[^:]+debug_test[^:]+$`
+	actual := d.File()
+
+	match, _ := regexp.MatchString(expected, actual)
+	if !match {
+
+		t.Errorf("result of calling `File()` - '%s' - did not match expected format '%s'", actual, expected)
+	}
+}
+
+func Test_FileLine(t *testing.T) {
+
+	// NOTE: this regex will likely not work on Windows
+
+	expected := `^[^:]+debug_test[^:]+:\d+$`
+	actual := d.FileLine()
+
+	match, _ := regexp.MatchString(expected, actual)
+	if !match {
+
+		t.Errorf("result of calling `FileLine()` - '%s' - did not match expected format '%s'", actual, expected)
+	}
+}
+
+func Test_FileLineFunction(t *testing.T) {
+
+	// NOTE: this regex will likely not work on Windows
+
+	expected := `^[^:]+debug_test[^:]+:\d+:.*Test_FileLineFunction$`
+	actual := d.FileLineFunction()
+
+	match, _ := regexp.MatchString(expected, actual)
+	if !match {
+
+		t.Errorf("result of calling `FileLineFunction()` - '%s' - did not match expected format '%s'", actual, expected)
+	}
+}
