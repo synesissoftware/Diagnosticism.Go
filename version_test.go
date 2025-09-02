f8d7289c24ab49313178075bcf0b5a692b491095
Matt Wilson <matthew.wilson.synesis@gmail.com>
2025-09-03 07:58:18 +1000

    version

--- version_test.go
+++ version_test.go
@@ -12,7 +12,7 @@ const (
 	Expected_VersionMajor uint16 = 0
 	Expected_VersionMinor uint16 = 10
 	Expected_VersionPatch uint16 = 0
-	Expected_VersionAB    uint16 = 0x8001
+	Expected_VersionAB    uint16 = 0xFFFF
 )
 
 func Test_Version_Elements(t *testing.T) {
@@ -23,9 +23,9 @@ func Test_Version_Elements(t *testing.T) {
 }
 
 func Test_Version(t *testing.T) {
-	require.Equal(t, uint64(0x0000_000A_0000_8001), Version)
+	require.Equal(t, uint64(0x0000_000A_0000_FFFF), Version)
 }
 
 func Test_Version_String(t *testing.T) {
-	require.Equal(t, "0.10.0-beta1", VersionString())
+	require.Equal(t, "0.10.0", VersionString())
 }
